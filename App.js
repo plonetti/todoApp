969d4afb70f9b77b5a1a8ca8c3860f1483a72474
plonetti <lonetti@paciunzio.istruzioneer.it>
2024-01-24 22:37:04 +0100

    update

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, View, ScrollView , FlatList} from 'react-native';
 import {Appbar,Text, TextInput, Button, Card, List, IconButton} from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
 export default function App() {
 
 const [note, setNote] = useState("");
@@ -53,38 +52,39 @@ useEffect( ()=>{
 	},[]);
 	
   return (
-		<SafeAreaProvider>
+	<SafeAreaProvider>
 		<View style={styles.container}>
-		<Appbar.Header>
-			<Appbar.Content title="Note Giornaliere" />  
-		</Appbar.Header>
+			<Appbar.Header>
+				<Appbar.Content title="Note Giornaliere" />  
+			</Appbar.Header>
+			
+			<TextInput style={{backgroundColor:'white'}}
+				label="Aggiungi nota"
+				value={note} 
+				onChangeText={(note) => setNote(note) } 
+			/>
+			<Button mode="contained" onPress={()=>addNote()} style={{margin:5, backgroundColor:'#A52A2A'}}>
+				Salva
+			</Button>
+			<ScrollView>
+			
+			<View>
 		
-		<TextInput style={{backgroundColor:'white'}}
-		label="Aggiungi nota"
-		value={note} 
-		onChange={(note) => setNote(note.target.value) } 
-		/>
-		<Button mode="contained" onPress={()=>addNote()} style={{margin:5, backgroundColor:'#A52A2A'}}>
-			Salva
-		</Button>
-		<ScrollView>
-		<View>
-	
-		{notes.map ( (item, index) => 
-			(<Card key={index} style={{margin:5}}>               
-			  <List.Item
-				title={item+"XXX"}
-				right={() => <IconButton icon="delete"  onPress={()=>{deleteNotes(index)} } />}
-				/>       
-			</Card>             
+			{notes.map ( (item, index) => 
+				(<Card key={index} style={{margin:5}}>               
+				<List.Item
+					title={item}
+					right={() => <IconButton icon="delete"  onPress={()=>{deleteNotes(index)} } />}
+					/>       
+				</Card>             
+				)
 			)
-		  )
-	  	}
+			}
 
-		</View>  
-		</ScrollView>         
-		<StatusBar style="auto" />
-	</View>
+			</View>  
+			</ScrollView>         
+			<StatusBar style="auto" />
+		</View>
 	</SafeAreaProvider>
 	);
 }
--- text.js
+++ text.js
@@ -1,5 +1,37 @@
-<FlatList
+<SafeAreaProvider>
+		<View style={styles.container}>
+		<Appbar.Header>
+			<Appbar.Content title="Note Giornaliere" />  
+		</Appbar.Header>
+		
+		<TextInput style={{backgroundColor:'white'}}
+		label="Aggiungi nota"
+		value={note} 
+		onChangeText={(note) => setNote(note) } 
+		/>
+		<Button mode="contained" onPress={()=>addNote()} style={{margin:5, backgroundColor:'#A52A2A'}}>
+			Salva
+		</Button>
+	
+		
+		<FlatList
 			data={notes}
 			keyExtractor={(item,index)=>index.toString()}
 			renderItem={({item})=><Text>{item}</Text>}
 		/>
+
+	       
+		<StatusBar style="auto" />
+	</View>
+	</SafeAreaProvider>
+
+		{notes.map ( (item, index) => 
+			(<Card key={index} style={{margin:5}}>               
+			  <List.Item
+				title={item+"XXX"}
+				right={() => <IconButton icon="delete"  onPress={()=>{deleteNotes(index)} } />}
+				/>       
+			</Card>             
+			)
+		  )
+	  	}
